bb6e4dbe79e23586079262fbfa74142fa50a02ce
mattchew1010 <mattchewiscool@gmail.com>
2023-10-31 02:04:31 -0400

    beginner styling for create account page

--- app/routes/signup/route.js
+++ app/routes/signup/route.js
@@ -1,7 +1,16 @@
 
 
 export default function App() {
-   return(
-      <h1 className="text-white">Signup</h1>
+   return (
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div className="w-full rounded-lg shadow border sm:max-w-md xl:p-0 bg-neutral-800 border-neutral-700 space-y-5">
+          <h1 className="mx-6 mt-2 text-xl font-bold leading-tight tracking-tight md:text-2xl text-gray-300">
+            Create An Account
+         </h1>
+         <form className="mx-6 pb-6 space-y-5" action="#" method="POST">
+            <input type="username" name="username" id="username" className="my-0 bg-transparent border-0 outline-none text-white ring-2 ring-inset ring-indigo-600 rounded-md block w-full p-2.5" placeholder="Username" required=""/>
+         </form>
+      </div>
+  </div>
    )
 }
