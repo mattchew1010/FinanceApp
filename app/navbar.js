9ac3b540bd0bbbd2a53875067d4cc2eaf2d32fb4
mattchew1010 <mattchewiscool@gmail.com>
2023-11-14 02:29:36 -0500

    Add session authentication to navbar

--- app/navbar.js
+++ app/navbar.js
@@ -1,10 +1,38 @@
 import Account from "./components/accountNavbar"
 import NoAccount from "./components/noAccountNavbar"
 import { useLoaderData } from "@remix-run/react"
+import { sessionIdCookie } from "./cookies.server"
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+
+export async function loader({request}) {
+   const cookieHeader = request.headers.get("Cookie");
+   const cookie = (await sessionIdCookie.parse(cookieHeader)) || null
+
+   if (cookie && cookie.match(/[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[0-9a-f]{4}-[0-9a-f]{12}|[0-9a-f]{12}4[0-9a-f]{19}/)) {
+      const session = await prisma.session.findUnique({
+         where: {
+            id: cookie
+         }
+      })
+      if (session) {
+         const user = await session.user()
+         if (user) {
+            return { loggedIn: true, user: user}
+         }
+         return { loggedIn: false}
+      } else{
+         return { loggedIn: false }
+      }
+
+   }else { //invalid or no cookie
+      return { loggedIn: false }
+   }
+}
 
 export default function Navbar() {
    const data = useLoaderData()
-   
+   const loggedIn = data ? data.loggedIn : false
    return (
     <nav className="bg-neutral-800">
    <div className="mx-0 px-2 sm:px-6 lg:px-8">
@@ -23,7 +51,7 @@ export default function Navbar() {
          </div>
          </div>
          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            {data === null ? <NoAccount/> : <Account />}
+            {loggedIn === false ? <NoAccount/> : <Account />}
          </div>
       </div>
    </div>
