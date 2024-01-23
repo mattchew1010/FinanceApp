9b4d8497c887bf9b8c8aea2b588a11a73b20b456
mattchew1010 <mattchewiscool@gmail.com>
2024-01-23 04:44:46 -0500

    Add functionality to add new tips

--- app/dashboard/tips/page.js
+++ app/dashboard/tips/page.js
@@ -2,6 +2,7 @@ import Table from './table'
 
 import { PrismaClient } from "@prisma/client"
 import {cookies} from "next/headers"
+import {redirect} from "next/navigation"
 
 const prisma = new PrismaClient()
 const itemsPerPage = 10
@@ -34,15 +35,37 @@ export async function getTips() {
             sessionId: sessionCookie.value
         }
     }).user({select: {_count: {select: {tips: true}}}})
-    console.log("tips",tipItems, tipCount)
+    console.log("tips", tipCount)
     return {list: tipItems, count: tipCount._count.tips}
+}
+
+export async function addTip(formData){
+    "use server"
+    const sessionCookie = cookies().get("session")
+    if (sessionCookie == null) {
+        return redirect("/")
+    }
+    const user = await prisma.session.findUnique({
+        where: {
+            sessionId: sessionCookie.value
+        }
+    }).user()
+    const tip = await prisma.tip.create({
+        data: {
+            date: new Date(formData.date),
+            hoursWorked: Number(formData.hoursWorked),
+            amount: Number(formData.amount),
+            userId: user.id
+        }
+    })
+    redirect("/dashboard/tips")
 }
 
 export default async function dashboard() {
     const rows = await getTips()
    return (
-   <div class="grid grid-cols-1 gap-4 m-7">
-        <Table tipRows={rows}/>
+   <div class="grid grid-cols-1 gap-4 m-7 justify-center">
+        <Table tipRows={rows} addTip={addTip}/>
    </div>
    )    
 }
--- app/dashboard/tips/table.js
+++ app/dashboard/tips/table.js
@@ -1,42 +1,56 @@
 "use client"
 import { useState } from "react"
-export default function table({tipRows}) {
+export default function table({tipRows, addTip}) {
     
     const [newTip, setNewTip] = useState({date: "", hoursWorked: "", amount: "", open: false})
     console.log(tipRows)
    return (
-      <div class="bg-neutral-800 w-full rounded-md">
-            <div class="flex flex-row justify-end items-center px-4 py-2">
-                <button onClick={() => {
-                    setNewTip({...newTip, open: !newTip.open})
-                }} class="bg-violet-700 text-white hover:bg-violet-800 hover:text-white rounded-md px-3 py-2 text-md font-medium">
-                    + Add
-                </button>
-            </div>
-            <table class="w-full text-md text-left font-medium text-gray-400">
-                <thead class="text-md text-gray-300 uppercase bg-neutral-800">
+      <div class="bg-neutral-800 w-1/2 justify-self-center rounded-md">
+            <table class="w-full text-md text-left rounded font-medium text-gray-400">
+                <thead class="text-md text-gray-300 uppercase bg-neutral-700">
                     <tr>
                         <th scope="col" class="px-2 py-3">Date</th>
                         <th scope="col" class="px-2 py-3">Hours</th>
                         <th scope="col" class="px-2 py-3">Tips</th>
-                        <th scope="col" class="px-2 py-3">
-                        <span class="sr-only">Actions</span>
-                        </th>
+                        <button onClick={() => {
+                            setNewTip({...newTip, open: !newTip.open})
+                        }} class="bg-violet-700 text-white hover:bg-violet-800 hover:text-white rounded-md px-3 py-2 text-md font-medium">
+                            + Add
+                        </button>
                     </tr>
                 </thead>
-                {newTip.open && (
-                    <tr>
-                        <th scope="row" class="px-2 py-3 font-medium whitespace-nowraptext-white">
-                            {newTip.date}
-                        </th>
-                        <td class="px-2 py-3">{newTip.hoursWorked}</td>
-                        <td class="px-2 py-3">${newTip.amount}</td>
-                    </tr>
-                )}
-                
+                {
+                    newTip.open && (
+                        <tr class="border-b border-gray-400">
+                            <th scope="row" class="px-2 py-3 font-medium whitespace-nowraptext-white">
+                                <input class="bg-neutral-700 placeholder-gray-500 focus:border-blue-500 border-neutral-500 border rounded-lg p-1.5" type="date" onChange={(e) => {
+                                    setNewTip({...newTip, date: e.target.value})
+                                }}/>
+                            </th>
+                            <td class="px-2 py-3">
+                                <input class="bg-neutral-700 placeholder-gray-500 focus:border-blue-500 border-neutral-500 border rounded-lg p-1.5" placeholder="0" type="number" onChange={(e) => {
+                                    setNewTip({...newTip, hoursWorked: e.target.value})
+                                }}/>
+                            </td>
+                            <td class="px-2 py-3">
+                                <input class="bg-neutral-700 placeholder-gray-500 focus:border-blue-500 border-neutral-500 border rounded-lg p-1.5" placeholder="$0" type="number" onChange={(e) => {
+                                    setNewTip({...newTip, amount: e.target.value})
+                                }}/>
+                            </td>
+                            <td class="px-2 py-3">
+                                <button onClick={() => {
+                                    addTip(newTip)
+                                    setNewTip({...newTip, open: false})
+                                }} class="bg-violet-700 text-white hover:bg-violet-800 hover:text-white rounded-md px-3 py-2 text-md font-medium">
+                                    Save
+                                </button>
+                            </td>
+                        </tr>
+                    )
+                }
                 {tipRows.list.map((row) => {
                     return (
-                        <tr>
+                        <tr class="border-b border-gray-400">
                             <th scope="row" class="px-2 py-3 font-medium whitespace-nowraptext-white">
                                 {row.date.toLocaleDateString()}
                             </th>
@@ -46,31 +60,6 @@ export default function table({tipRows}) {
                     )
                 })}      
             </table>
-            <dialog open class="bg-neutral-800 w-1/3 h-1/3 rounded-md p-4 outline outline-1 outline-violet-700 text-gray-300">
-                <h1 class="text-center text-xl font-medium">Add Tip</h1>
-                <form action="/dashboard/tip" class="flex flex-col justify-center items-center my-2 ">
-                    <div class="flex flex-row justify-between items-center w-full my-2">
-                        <label class="text-lg font-medium">Date</label>
-                        <input onChange={(e) => setNewTip({...newTip, date: e.target.value})} class="w-1/2 rounded-md bg-neutral-800 outline outline-1 outline-violet-700 text-gray-300" type="date" name="date" id="date"/>
-                    </div>
-                    <div class="flex flex-row justify-between items-center w-full my-2">
-                        <label class="text-lg font-medium">Hours Worked</label>
-                        <input onChange={(e) => setNewTip({...newTip, hoursWorked: e.target.value})} class="w-1/2 rounded-md bg-neutral-800 outline outline-1 outline-violet-700 text-gray-300" type="number" step="0.01" name="hoursWorked" id="hoursWorked"/>
-                    </div>
-                    <div class="flex flex-row justify-between items-center w-full my-2">
-                        <label class="text-lg font-medium">Amount</label>
-                        <input onChange={(e) => setNewTip({...newTip, amount: e.target.value})} class="w-1/2 rounded-md bg-neutral-800 outline outline-1 outline-violet-700 text-gray-300" type="number" step="1" name="amount" id="amount"/>
-                    </div>
-                    <div class="flex flex-row justify-between items-center w-full my-2">
-                        <button onClick={() => setNewTip({...newTip, open: false})} class="bg-violet-700 text-white hover:bg-violet-800 hover:text-white rounded-md px-3 py-2 text-md font-medium">
-                            Cancel
-                        </button>
-                        <button type="submit" class="bg-violet-700 text-white hover:bg-violet-800 hover:text-white rounded-md px-3 py-2 text-md font-medium">
-                            Add
-                        </button>
-                    </div>
-                </form>
-            </dialog>
         </div>
    )
 }
